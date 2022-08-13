80bbd84f7e3b6abd1737d76af81d8fb4f3e6ee87
Sourabh Singh <Sourabh@iiitmanipur.ac.in>
2022-08-13 16:50:20 +0530

    first commit

---
+++ src/Firebase.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBe2DK9rXVamToanfegfe4mtwjfD2c-fFw",
+  authDomain: "bi-culur.firebaseapp.com",
+  projectId: "bi-culur",
+  storageBucket: "bi-culur.appspot.com",
+  messagingSenderId: "528713536106",
+  appId: "1:528713536106:web:67a9684c66aac4691f3013",
+  measurementId: "G-7SETD4NYDQ"
+};
+
+
+export const app = initializeApp(firebaseConfig);
